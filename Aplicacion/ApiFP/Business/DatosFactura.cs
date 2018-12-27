426c3b8c2e0cf721a31f21ec6ee48a1a1eda5aee
guido albarello <guido.albarello@sarsy-srl.com>
2018-12-27 12:04:13 -0300

    Add paquete itextsharp. Se crea carpeta Business para guardar lógica de negocios y template DatosFactura. Se crea el template del servicio PDFParser

---
+++ Aplicacion/ApiFP/Business/DatosFactura.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiFP.Business
+{
+    public class DatosFactura {
+
+    }
+}
---
+++ Aplicacion/ApiFP/Services/PDFParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace ApiFP.Services
+{
+    public class PDFParser {
+        public Business.DatosFactura extraerCamposDePDF(MemoryStream aPdfStream) {
+            PdfReader pdfReader = new PdfReader(aPdfStream);
+            Business.DatosFactura datosExtraidos = new Business.DatosFactura();
+            pdfReader.Close();
+            return datosExtraidos;
+        }
+    }
+}
