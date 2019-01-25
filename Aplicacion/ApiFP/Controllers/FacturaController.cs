0840ae758a3acae5fee6faa74a8dd79620756a54
diego serra <diego.serra@gmail.com>
2019-01-25 16:52:31 -0300

    se incorpora framework de log (log4net)

--- Aplicacion/ApiFP/Controllers/FacturaController.cs
+++ Aplicacion/ApiFP/Controllers/FacturaController.cs
@@ -26,6 +26,7 @@ namespace ApiFP.Controllers
         [HttpPost]
         public async Task<IHttpActionResult> CreateFactura(CreateFacturaBindingModel createFacturaModel)
         {
+            log.Debug("");
 
             if (!createFacturaModel.ValidarSinArchivo())
             {
